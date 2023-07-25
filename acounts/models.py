9b7c26a584abdc03967c210d96af81dc7ba8a813
beba2123 <Antenehbeba@gmail.com>
2023-07-26 00:15:01 +0300

    html updated commit

--- acounts/models.py
+++ acounts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+# from django.db import models
+# import uuid
 
-# Create your models here.
+# # Create your models here.
+# class product_type(models.Model):
+#  id = models.UUIDField(default=uuid.uuid4, unique=True,
+#                          primary_key=True, editable=False)
+#  type = models.CharField(max_length=200, blank=False, null=False)
+
+# class Product(models.Model):
+#     id = models.UUIDField(default=uuid.uuid4, unique=True,
+#                          primary_key=True, editable=False)
+#     name = models.CharField(max_length=200, blank=False, null=False)
+#     price = models.IntegerField(default=0, blank=False, null=False)
+#     quantity = models.IntegerField(default = 0, blank=False, null = False)
+#     date_created = models.DateTimeField(auto_now_add=True)
+    
+#     def __str__(self):
+#         return self.name;
+
+
