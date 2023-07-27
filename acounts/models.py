d1aab128239886459b556d0431f0df6dfe015441
beba2123 <Antenehbeba@gmail.com>
2023-07-27 14:02:00 +0300

    create and update a table on model file

--- acounts/models.py
+++ acounts/models.py
@@ -2,22 +2,44 @@ from django.db import models
 import uuid
 from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
-class customer(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=200, blank=True, null=True)
-    phone_number = PhoneNumberField()
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
     Email = models.EmailField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
-# class Product(models.Model):
-#     id = models.UUIDField(default=uuid.uuid4, unique=True,
-#                          primary_key=True, editable=False)
-#     name = models.CharField(max_length=200, blank=False, null=False)
-#     price = models.IntegerField(default=0, blank=False, null=False)
-#     quantity = models.IntegerField(default = 0, blank=False, null = False)
-#     date_created = models.DateTimeField(auto_now_add=True)
+
+class Tags(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name;
+
+class Product(models.Model):
+    CATAGORY = (
+        ('Indoor', 'indoor'),
+        ('Out-Door', 'Out-Door'),
+    )
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                         primary_key=True, editable=False)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    price = models.FloatField(default=0, blank=False, null=False)
+    category = models.CharField(max_length=200, blank=True, null=False, choices=CATAGORY)
+    description = models.CharField(max_length=200, null=True)
+    quantity = models.IntegerField(default = 0, blank=False, null = False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tags)
     
-#     def __str__(self):
-#         return self.name;
-
-
+    def __str__(self):
+        return self.name;
+class Order(models.Model):
+    STATUS = (
+            ('Pending', 'Pending'),
+            ('Out for delivery', 'out for delivery'),
+            ('Delivered', 'Delivered'),
+            )
+    customer  = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200,null=True, choices=STATUS)
