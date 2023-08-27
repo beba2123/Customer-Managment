47200544f0a90cf0a8856634ec716b8e17ec7e80
beba2123 <antenehbeba@gmail.com>
2023-08-27 15:14:48 +0300

    configuring  media root for storing image

--- customerManagment/settings.py
+++ customerManagment/settings.py
@@ -124,6 +124,9 @@ MEDIA_URL = '/image/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- customerManagment/urls.py
+++ customerManagment/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('acounts.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # when ever you go to media url it redirect you  to the media root inorder to  find the image.
