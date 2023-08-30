b15b26c05d1e0921cbcd9aa900f873593b7daa5e
beba2123 <antenehbeba@gmail.com>
2023-08-30 14:08:36 +0300

    importing signals.py into apps & check it

--- acounts/apps.py
+++ acounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AcountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'acounts'
+
+    def ready(self):
+        import acounts.signals
