ac516a28a300510900845f04ea8be93b46b1fc62
beba2123 <antenehbeba@gmail.com>
2023-09-26 15:57:39 +0300

    create reset password html & connect to url file

--- acounts/urls.py
+++ acounts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('user_profile', views.user_page, name='user_profile'),
     path('acounts/', views.accountSettings, name="accounts"),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset-password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="acounts/password_reset.html"), name="reset-password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_successfully', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
