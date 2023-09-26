68720838cfb738b03ee0f685b9a72886d90f283c
beba2123 <antenehbeba@gmail.com>
2023-09-26 15:50:27 +0300

    update my host and password for smtp configuration

--- customerManagment/settings.py
+++ customerManagment/settings.py
@@ -131,3 +131,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER= 'beboobeba0911@gmail.com' # Your email address
+EMAIL_HOST_PASSWORD='nnihwmrdeurgrnvl'   # Password of your Email Address
+
